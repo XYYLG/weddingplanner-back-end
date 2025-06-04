94cb8335ca78c7d9536dda1639e8a406c18841de
XYYLG <547214@student.fontys.nl>
2025-06-04 13:48:15 +0200

    WIP WS

--- src/ws/websocket.server.ts
+++ src/ws/websocket.server.ts
@@ -4,25 +4,26 @@ import { CreateEditGuestDto } from "src/modules/guests/Dto/create-edit-guest.dto
 import { GuestService } from "src/modules/guests/guest.service";
 
 
-export function setupWebSocketServer(app: INestApplicationContext) {
+let wssGlobal: WebSocketServer;
 
+export function setupWebSocketServer(app: INestApplicationContext) {
     const wss = new WebSocketServer({ port: 8082 });
+    wssGlobal = wss; //  opslaan in globale variabele
 
     const guestService = app.get(GuestService);
+    guestService.setWebSocketServer(wss); //  doorgeven aan service
 
-    // Verwerk inkomende connecties
     wss.on('connection', (ws: WebSocket) => {
         console.log('WebSocket client verbonden');
 
         ws.on('message', async (data) => {
             try {
                 const parsed: CreateEditGuestDto = JSON.parse(data.toString());
-
                 const guest = await guestService.createGuest(parsed);
 
+                // Alleen naar deze client
                 ws.send(JSON.stringify({ success: true, guest }));
             } catch (err) {
-
                 ws.send(JSON.stringify({ success: false, error: (err as Error).message }));
             }
         });
