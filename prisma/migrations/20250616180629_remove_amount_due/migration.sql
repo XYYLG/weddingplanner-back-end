cea44b23edafb2694b61f21339aa329d7cfa2600
XYYLG <547214@student.fontys.nl>
2025-06-16 23:53:40 +0200

    removed unnused migration

--- prisma/migrations/20250616180629_remove_amount_due/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `amountDue` on the `finance` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `finance` DROP COLUMN `amountDue`;
