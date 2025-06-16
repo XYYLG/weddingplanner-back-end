23a7dc3be4a749b4a3cbbea0d2f1255dbc605c95
XYYLG <547214@student.fontys.nl>
2025-06-16 10:17:55 +0200

    Updated appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GuestModule } from './modules/guests/guest.module';
 import { FinanceModule } from './modules/finance/finance.module';
+import { Auth0Module } from './modules/Auth0/auth0.module';
 
 @Module({
-  imports: [GuestModule, FinanceModule],
+  imports: [GuestModule, FinanceModule, Auth0Module],
   controllers: [AppController],
   providers: [AppService],
 })
