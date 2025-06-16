5a20c5fdc949c532b057eb4e1209a8f9e95a8d3b
XYYLG <547214@student.fontys.nl>
2025-06-16 10:38:55 +0200

    Fixed auth0 with googleAPI

--- src/modules/Auth0/auth0.controller.ts
+++ src/modules/Auth0/auth0.controller.ts
@@ -7,13 +7,20 @@ export class Auth0Controller {
 
     @Get('idp-token/:userId')
     async getIdpToken(@Param('userId') userId: string) {
-        const user = await this.auth0Service.getUserById(userId);
+        console.log(`GET /auth0/idp-token/${userId} aangeroepen`);
+
+        const response = await this.auth0Service.getUserById(userId);
+        const user = response.data;  // <== fix hier
+
+        console.log('Auth0 user:', user);
 
         const token = user?.identities?.[0]?.access_token;
         if (!token) {
+            console.log('Geen IdP access token gevonden');
             return { error: 'No IdP access token found' };
         }
-
+        console.log('IdP access token:', token);
         return { accessToken: token };
     }
+
 }
